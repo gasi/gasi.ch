1441eb7fffdb28cf71d3dd31a45362a91ee5eeeb
Daniel Gasienica <daniel@gasienica.ch>
2012-05-17 22:16:18 -0700

    Add ‘Packartis’ portfolio project.

---
+++ public/portfolio/packartis/getflash.php
@@ -0,0 +1,29 @@
+<?php echo "<?xml version=\"1.0\" encoding=\"utf-8\"?".">"; ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>packartis AG | flexible. And packaging. | GET FLASH</title>
+<meta name="robots"                 content="noindex, nofollow" />
+<meta name="robots"                 content="all" />
+<meta name="title"                  content="packartis AG" />
+<meta name="author"                 content="dg:design" />
+<meta name="publisher"              content="dg:design" />
+<meta name="copyright"              content="packartis AG, 2002" />
+<meta name="language"               content="deutsch, de" />
+<meta name="keywords"               content="Packartis AG/SA/Ltd, Schweiz, France, Vakuum, Gasschutzverpackungsmaterial, complexes emballages, sousvides, sous atmospère modifiée, films, vacuum-and MAP-Packaging, Flexible films, Vacuumpouches, sachets sousvides, Printed laminates, complexes multicouches imprimés" />
+<meta name="description"            content="Packartis AG - Mehrschichtfolien-Spezialisten für Vakuum- und Gasschutzverpackungen, bedruckt und transparent, für frische Lebensmittel und techn. Artikel. - Tel. +41 1 451 77 50" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="assets/css/packartis_global.css" rel="stylesheet" type="text/css" />
+</head>
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#cccccc">
+<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#cccccc">
+  <tr>
+    <td align="center" valign="middle"><div align="center"><img align="center" src="assets/images/packartis_getflash.gif" width="480" height="320" border="0" usemap="#Map"></div></td>
+  </tr>
+</table>
+<map name="Map" id="Map">
+  <area shape="rect" coords="29,168,231,196" href="http://www.macromedia.com/go/getflashplayer/" target="_blank" alt="Get Flash Player 6,0,79,0" />
+  <area shape="rect" coords="29,199,231,225" href="flashindex.php" target="_self" alt="BACK" />
+  <area shape="rect" coords="28,269,231,296" href="http://www.google.com/" target="_self" alt="EXIT" />
+</map>
+</body>
+</html>
